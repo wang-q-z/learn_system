fe43e89b2410070135a1208ae54b9cf5b1b87efc
wang-q-z <2415767706@qq.com>
2022-12-02 23:20:36 +0800

    12.2

--- csapp/L3/ASS/src/memory/instruction.c
+++ csapp/L3/ASS/src/memory/instruction.c
@@ -1,3 +1,21 @@
-//#include
+#include"memory/instruction.h"
 
-//uint64_t decode_od(od_t od)
+uint64_t decode_od(od_t od)
+{
+    if(od.type == IMM)
+    {
+        return od.imm;
+    }
+    else if(od.type == REG)
+    {
+        /*
+        reg.rsp pmm[]
+        */
+        return (uint64_t)od.reg1;
+
+    }
+    else
+    {
+        
+    }
+}
